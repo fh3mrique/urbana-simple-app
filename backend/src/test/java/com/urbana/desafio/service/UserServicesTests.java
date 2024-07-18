6ce98b3a50ef334166edbb075d2715a3609546a3
Filipe Lima <fhemrique55@gmail.com>
2024-07-17 21:49:44 -0300

    test: add test for delete method in UserService

--- backend/src/test/java/com/urbana/desafio/service/UserServicesTests.java
+++ backend/src/test/java/com/urbana/desafio/service/UserServicesTests.java
@@ -8,13 +8,18 @@ import com.urbana.desafio.domain.entities.User;
 import com.urbana.desafio.domain.repositories.BoardingPassTypeRepository;
 import com.urbana.desafio.domain.repositories.UserRepository;
 import com.urbana.desafio.services.UserService;
+import com.urbana.desafio.services.exceptions.DatabaseExceptions;
+import com.urbana.desafio.services.exceptions.ResourcesNotFoundExceptions;
 import com.urbana.desafio.utils.UserFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Arrays;
@@ -23,7 +28,9 @@ import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -38,9 +45,17 @@ public class UserServicesTests {
     @Mock
     private BoardingPassTypeRepository boardingPassTypeRepository;
 
+    private Long idExistente;
+    private Long idNaoExistente;
+    private Long idDependente;
+
     @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    void setUp() throws Exception {
+        idExistente = 1L;
+        idNaoExistente = 1000L;
+        idDependente = 4L;
+
+        /*dto = Factory.criarProdutoDTO();*/
     }
 
     @Test
@@ -73,7 +88,6 @@ public class UserServicesTests {
 
         User userEntity = UserFactory.createUser(null, "John Doe", "john.doe@example.com", "password123", boardingPassTypes);
 
-        // Configurar os mocks
         when(boardingPassTypeRepository.findById(type1.getId())).thenReturn(Optional.of(type1));
         when(boardingPassTypeRepository.findById(type2.getId())).thenReturn(Optional.of(type2));
         when(repository.save(any(User.class))).thenReturn(userEntity);
@@ -86,6 +100,35 @@ public class UserServicesTests {
 /*
         assertEquals(userInsertDTO.boardingPassTypes().size(), result.boardingPassTypes().size());
 */
+    }
+
+    @Test
+    public void deleteShouldFazerNadaWhenIdExistir() {
+        Assertions.assertDoesNotThrow(()->{
+            repository.deleteById(idExistente);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(idExistente);
+    }
+
+    @Test
+    public void deleteShouldLancaDatabaseExceptionWhenIdForDependente() {
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(idDependente);
+
+        assertThrows(DatabaseExceptions.class, () -> {
+            service.delete(idDependente);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(idDependente);
+    }
+
+    @Test
+    public void shouldThrowResourcesNotFoundExceptionsWhenDeletingNonExistentId() {
+        doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(idNaoExistente);
+
+        assertThrows(ResourcesNotFoundExceptions.class, () -> {
+            service.delete(idNaoExistente);
+        });
     }
 
 
