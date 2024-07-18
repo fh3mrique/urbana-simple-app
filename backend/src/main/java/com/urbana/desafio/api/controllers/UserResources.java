51985439497b5d2adcef1dfe4578b283dd415477
Filipe Lima <fhemrique55@gmail.com>
2024-07-17 22:15:29 -0300

    feat: endpoints insert, update and delete

--- backend/src/main/java/com/urbana/desafio/api/controllers/UserResources.java
+++ backend/src/main/java/com/urbana/desafio/api/controllers/UserResources.java
@@ -1,12 +1,13 @@
 package com.urbana.desafio.api.controllers;
 
 import com.urbana.desafio.api.dtos.UserDTO;
+import com.urbana.desafio.api.dtos.UserInsertDTO;
 import com.urbana.desafio.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,31 @@ public class UserResources {
     public ResponseEntity<List<UserDTO>> findAll(){
         List<UserDTO> users = service.findAll();
         return  ResponseEntity.ok().body(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserInsertDTO> insert(@RequestBody UserInsertDTO dto){
+
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.id()).toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserInsertDTO> update(@PathVariable Long id, @RequestBody UserInsertDTO dto) {
+
+        dto = service.update(id, dto);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
