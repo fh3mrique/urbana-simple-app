d1aca4ca5b2ed38753443a4603f15b87087204b7
Filipe Lima <fhemrique55@gmail.com>
2024-07-20 16:22:29 -0300

    feat: focus invalid input

--- frontend/src/app/components/users-form/users-form.component.ts
+++ frontend/src/app/components/users-form/users-form.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ElementRef } from '@angular/core';
 import { IUser } from '../../interfaces/user/user.interface';
 import { UserService } from '../../services/users.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-users-form',
@@ -9,11 +12,14 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './users-form.component.scss'
 })
 export class UsersFormComponent implements OnInit, OnChanges {
+
   @Input() userSelected: IUser = {} as IUser;
+
+  @Output('onFormSubmit') onFormSubmitEmitt = new EventEmitter<void>
 
   displayedColumns: string[] = ['id', 'typeBoardingPass', 'number', 'status', 'actions' ];
 
-  constructor(private userService: UserService, private snackBar: MatSnackBar) {}
+  constructor(private userService: UserService, private snackBar: MatSnackBar, private readonly _elRef: ElementRef) {}
   
   
   ngOnInit() {
@@ -32,6 +38,29 @@ export class UsersFormComponent implements OnInit, OnChanges {
         return 'https://www.urbana-pe.com.br/wp-content/uploads/2019/09/cartao-vem-estudante.png';
       default:
         return 'path/to/default-image.png';
+    }
+  }
+
+  onFormSubmit(form: NgForm) {
+    if(form.invalid) {
+      this.focusOnInvalidControl(form);
+
+      return;
+    }
+
+    console.log('VALIDO');
+    this.onFormSubmitEmitt.emit();
+  }
+
+  focusOnInvalidControl(form: NgForm) {
+    for (const control of Object.keys(form.controls)) {
+      if(form.controls[control].invalid) {
+        const invalidControl: HTMLElement = this._elRef.nativeElement.querySelector(`[name=${control}]`);
+
+        invalidControl.focus();
+
+        break;
+      }
     }
   }
 
