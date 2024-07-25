67b3c7a026b622b8437a736e713c6e4ba57db10c
Filipe Lima <fhemrique55@gmail.com>
2024-07-25 17:13:59 -0300

    feat: boading pass image pipe

--- frontend/src/app/components/user/user-update/user-update.component.ts
+++ frontend/src/app/components/user/user-update/user-update.component.ts
@@ -56,19 +56,7 @@ export class UserUpdateComponent implements OnInit {
     })
   }
 
-  getImageForType(type: string): string {
-    switch (type) {
-      case 'TRABALHADOR':
-        return 'https://www.urbana-pe.com.br/wp-content/uploads/2019/09/cartao-vem-trabalhador.png';
-      case 'COMUM':
-        return 'https://www.urbana-pe.com.br/wp-content/uploads/2019/09/cartao-vem-comum.png';
-      case 'ESTUDANTE':
-        return 'https://www.urbana-pe.com.br/wp-content/uploads/2019/09/cartao-vem-estudante.png';
-      default:
-        return 'path/to/default-image.png';
-    }
-  }
-
+ 
   toggleBoardingPassStatus(pass: any): void {
     const newStatus = !pass.status;
     if (newStatus) {
--- frontend/src/app/pipes/pipes.module.ts
+++ frontend/src/app/pipes/pipes.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusLabelPipe } from './status-label.pipe';
+import { BoardingPassImagePipe } from './boarding-pass-image.pipe';
 
 
 
 @NgModule({
   declarations: [
-    StatusLabelPipe
+    StatusLabelPipe,
+    BoardingPassImagePipe
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    StatusLabelPipe
+    StatusLabelPipe,
+    BoardingPassImagePipe
   ]
 })
 export class PipesModule { }
